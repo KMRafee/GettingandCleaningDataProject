d3f2ba9f83e11ea53d66f16b0dcda84036703e31
KMRafee <rafee_tcs@yahoo.com>
2016-02-27 14:24:05 -0600

    changes to run analysis code

--- run_analysis.R
+++ run_analysis.R
@@ -132,6 +132,6 @@ tidyData    = merge(tidyData,activityType,by='activityId',all.x=TRUE)
 
 
 # Export the tidyData set 
-write.table(tidyData, './UCI HAR Dataset/tidyData.txt',row.names=TRUE,sep='\t')
+write.table(tidyData, './UCI HAR Dataset/tidyData.txt',row.names=FALSE,sep='\t')
 
 
